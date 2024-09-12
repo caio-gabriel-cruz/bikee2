f05b8a82bcfe66ecd13066a93100a9f8f129faec
caio-gabriel-cruz <killua37pt@gmail.com>
2024-09-12 20:18:26 -0300

    criação de ações pros botões

--- app/src/main/java/br/com/etecia/myapp/CadastroActivity.java
+++ app/src/main/java/br/com/etecia/myapp/CadastroActivity.java
@@ -1,7 +1,6 @@
 package br.com.etecia.myapp;
 
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,35 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.google.android.material.button.MaterialButton;
-
-public class LoginActivity extends AppCompatActivity {
-    //declarar as variáveis globais
-    MaterialButton btnRecuperar, btnCadastrar, btnEntrar;
+public class CadastroActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.login_layout);
+        setContentView(R.layout.cadastro_layout);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        //apresentando as variáveis do java para o xml
-        btnCadastrar = findViewById(R.id.btnCadastrar);
-        btnRecuperar = findViewById(R.id.btnRecuperar);
-        btnEntrar = findViewById(R.id.btnEntrar);
-
-        //criando as ações dos botões
-        btnCadastrar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
     }
 }
