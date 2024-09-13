fce08914152b156dbbb68957fb88760f3d6dfc01
caio-gabriel-cruz <killua37pt@gmail.com>
2024-09-12 22:07:00 -0300

    criação de ações pros botões

--- app/src/main/java/br/com/etecia/myapp/LoginActivity.java
+++ app/src/main/java/br/com/etecia/myapp/LoginActivity.java
@@ -3,6 +3,7 @@ package br.com.etecia.myapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,10 +12,13 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class LoginActivity extends AppCompatActivity {
     //declarar as variáveis globais
     MaterialButton btnRecuperar, btnCadastrar, btnEntrar;
+    TextInputEditText txtEmail, txtSenha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,8 +35,27 @@ public class LoginActivity extends AppCompatActivity {
         btnCadastrar = findViewById(R.id.btnCadastrar);
         btnRecuperar = findViewById(R.id.btnRecuperar);
         btnEntrar = findViewById(R.id.btnEntrar);
+        txtEmail = findViewById(R.id.txtEmail);
+        txtSenha = findViewById(R.id.txtSenha);
 
         //criando as ações dos botões
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email, password;
+
+                email = txtEmail.getText().toString().trim();
+                password = txtSenha.getText().toString().trim();
+
+                if (email.equals("etecia") && password.equals("etecia")){
+                    startActivity(new Intent(getApplicationContext(), MenuActivity.class));
+                    finish();
+                } else{
+                    Toast.makeText(getApplicationContext(), "Usuário ou senha inválidos", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
         btnCadastrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
