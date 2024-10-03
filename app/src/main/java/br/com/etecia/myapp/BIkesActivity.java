06379fd2729178acb96ef5f84d657cc4eb35cdfc
drakin9 <gui.olialves2@gmail.com>
2024-10-03 20:16:11 -0300

    2

--- app/src/main/java/br/com/etecia/myapp/BIkesActivity.java
+++ app/src/main/java/br/com/etecia/myapp/BIkesActivity.java
@@ -7,8 +7,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class BIkesActivity extends AppCompatActivity {
+    TabLayout idTabLayoutBikes;
+    ViewPager2 idViewPagerBike;
+    AdapterViewPagerBikes adapterViewPagerBikes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +26,30 @@ public class BIkesActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        idTabLayoutBikes = findViewById(R.id.idTabLayoutBikes);
+        idViewPagerBike = findViewById(R.id.idViewPagerBike);
+
+        adapterViewPagerBikes = new AdapterViewPagerBikes(this);
+
+        idViewPagerBike.setAdapter(adapterViewPagerBikes);
+
+        idTabLayoutBikes.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+
     }
 }
