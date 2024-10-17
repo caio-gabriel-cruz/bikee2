5b51348863ba0c68987f6aafb8d869d205b8510b
caio-gabriel-cruz <killua37pt@gmail.com>
2024-10-17 20:16:45 -0300

    tu tá aqui de novo menor

--- app/src/main/java/br/com/etecia/myapp/RecyclerAdapterBikeNova.java
+++ app/src/main/java/br/com/etecia/myapp/RecyclerAdapterBikeNova.java
@@ -1,5 +1,7 @@
 package br.com.etecia.myapp;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -9,23 +11,42 @@ import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.List;
+
 public class RecyclerAdapterBikeNova extends RecyclerView.Adapter<RecyclerAdapterBikeNova.ViewHolder> {
 
+    //criando variáveis globais para representar a lista e o contexto
+    private Context context;
+    private List<BikeNova> lstBikeNova;
+
+    //criando construtor
+
+    public RecyclerAdapterBikeNova(Context context, List<BikeNova> lstBikeNova) {
+        this.context = context;
+        this.lstBikeNova = lstBikeNova;
+    }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        view = inflater.inflate(R.layout.modelo_bikes_novas, parent, false);
+        return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.idModeloTituloBikeNova.setText(lstBikeNova.get(position).getTitulo());
+        holder.idModeloImagemBikeNova.setImageResource(lstBikeNova.get(position).getImage());
+
+        //criar o click do card
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return lstBikeNova.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
