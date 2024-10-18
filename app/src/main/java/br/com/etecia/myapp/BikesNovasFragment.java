05513a440f3a0ecc46558b5761f5083305b8a4d0
caio-gabriel-cruz <killua37pt@gmail.com>
2024-10-17 21:11:08 -0300

    tu tá aqui de novo menor

--- app/src/main/java/br/com/etecia/myapp/BikesNovasFragment.java
+++ app/src/main/java/br/com/etecia/myapp/BikesNovasFragment.java
@@ -3,18 +3,41 @@ package br.com.etecia.myapp;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class BikesNovasFragment extends Fragment {
 
+    private List<BikeNova> lstBikeNova;
+    RecyclerView idRecBikeNovas;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
         View view = inflater.inflate(R.layout.fragment_bikes_novas, container, false);
+
+        idRecBikeNovas  = view.findViewById(R.id.idRecBikeNovas);
+
+        // carregando os objetos na lista
+        lstBikeNova = new ArrayList<>();
+        lstBikeNova.add(new BikeNova("bike1",R.drawable.bike1));
+        lstBikeNova.add(new BikeNova("bike2",R.drawable.bike2));
+        lstBikeNova.add(new BikeNova("bike3",R.drawable.bike3));
+        lstBikeNova.add(new BikeNova("bike4",R.drawable.bike4));
+        lstBikeNova.add(new BikeNova("bike5",R.drawable.bike5));
+        lstBikeNova.add(new BikeNova("bike6",R.drawable.bike6));
+        lstBikeNova.add(new BikeNova("bike7",R.drawable.bike7));
+        lstBikeNova.add(new BikeNova("bike8",R.drawable.bike8));
+        lstBikeNova.add(new BikeNova("bike9",R.drawable.bike9));
+        lstBikeNova.add(new BikeNova("bike10",R.drawable.bike10));
+
 
         return view;
 
